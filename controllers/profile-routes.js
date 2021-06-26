e891d4622e4541409d711879e5ac06d931e1072e
EntropyParadigm <32775097+EntropyParadigm@users.noreply.github.com>
2021-06-26 03:42:05 -0700

    Update profile-routes.js

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -2,59 +2,15 @@ const router = require('express').Router();
 const { Exercise, User, Workout } = require('../models');
 
 router.get('/', async (req, res) => {
-    try {
-      //const newUserData = await User.findByPk(1,{
- /*       include:[
-                {
-               model:User,through:{
-                 User_name, attributes:[]
-                  }
-                } 
-              ] */
-      //});
+    
         res.status(200).json({ message: 'Connected!' });
-        //res.status(200).json({ newUserData });
-         //console.log(newUserData);
-  
-    //   req.session.save(() => {
-    //     req.session.userId = newUser.id;
-    //     // req.session.email = newUser.email;
-    //     // req.session.loggedIn = true;
-  
-    //     res.json(newUser);
-    //   });
-    } catch (err) { 
-    console.log(err)
-    res.status(500).json(err);
-    } 
+         
   });
 
 router.get('/profile', async (req, res) => {
-    try {
-      //const newUserData = await User.findByPk(1,{
- /*       include:[
-                {
-               model:User,through:{
-                 User_name, attributes:[]
-                  }
-                } 
-              ] */
-      //});
+    
         res.status(200).json({ message: 'Connected!' });
-        //res.status(200).json({ newUserData });
-         //console.log(newUserData);
-  
-    //   req.session.save(() => {
-    //     req.session.userId = newUser.id;
-    //     // req.session.email = newUser.email;
-    //     // req.session.loggedIn = true;
-  
-    //     res.json(newUser);
-    //   });
-    } catch (err) { 
-    console.log(err)
-    res.status(500).json(err);
-    } 
+         
   });
 
   router.get('/Match', async (req, res) => {
