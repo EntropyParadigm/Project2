d708d661ca6872e23d8a7ab2ed1df5f91e290b80
EntropyParadigm <32775097+EntropyParadigm@users.noreply.github.com>
2021-06-26 01:24:42 -0700

    Update profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,3 +1,31 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+    const name = document.querySelector('#name').value;
+    const age = document.querySelector('#age').value;
+    const height = document.querySelector('#height').value;
+    const weight = document.querySelector('#weight').value;
+    // The following is a ternary operator. It checks to see if workouts is checked. If it is, it will return true, otherwise, it will return false.
+    const has_workouts = document.querySelector('#has_workouts:checked') ? true : false;
+    // Send fetch request to add a new workout
+    const response = await fetch(../api/exerciseRoute.js, {
+      method: 'POST',
+      body: JSON.stringify({
+        name,
+        age,
+        height,
+        weight,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+    //if the workout is added, the 'all' template will be rerendered
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert('Failed to add workout');
+    }
+  }
 // const newFormHandler = async (event) => {
 //   event.preventDefault();
 
