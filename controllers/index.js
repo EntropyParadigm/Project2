0db80d9ba7caae8371902587a433d55a2525b92c
EntropyParadigm <32775097+EntropyParadigm@users.noreply.github.com>
2021-06-26 04:33:38 -0700

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
 const apiRoutes = require('./api');
-const profileRoutes = require('./profile-routes.js');
 const mainRoutes = require('./mainRoutes');
-
+const profileRoutes = require('./profile-routes.js');
 
 router.use('/api', apiRoutes);
-router.use('/profile', profileRoutes);
 router.use('/', mainRoutes);
+router.use('/profile', profileRoutes);
+
 
 module.exports = router;
