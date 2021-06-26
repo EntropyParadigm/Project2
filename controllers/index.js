81f97a084df8b08ca29cd4c40f3629731214c27a
EntropyParadigm <32775097+EntropyParadigm@users.noreply.github.com>
2021-06-26 02:37:21 -0700

    Profile Route Adjustments

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const mainRoutes = require('./mainRoutes');
+const profileRoutes = require('./profile-routes.js');
 
 router.use('/', mainRoutes);
 router.use('/api', apiRoutes);
+router.use('/profile', profileRoutes);
 
 module.exports = router;
