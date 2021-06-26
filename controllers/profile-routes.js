d1e592e53cb03216cb4fda5297bd7b264218a19c
EntropyParadigm <32775097+EntropyParadigm@users.noreply.github.com>
2021-06-26 02:47:08 -0700

    Create profile-routes.js

---
+++ controllers/profile-routes.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const { Exercise, User, Workout } = require('../models');
+
+router.get('/', async (req, res) => {
+    try {
+      const newUser = await User.findByPk(1,{
+        include:[
+                {
+               model:User,through:{
+                 User_name, attributes:[]
+                  }
+                } 
+              ] 
+      });
+      res.status(200).json({ newUser });
+  
+    //   req.session.save(() => {
+    //     req.session.userId = newUser.id;
+    //     // req.session.email = newUser.email;
+    //     // req.session.loggedIn = true;
+  
+    //     res.json(newUser);
+    //   });
+    } catch (err) { 
+    console.log(err)
+    res.status(500).json(err);
+    } 
+  });
+
+router.get('/profile', async (req, res) => {
+    try {
+      const newUser = await User.findByPk(1,{
+        include:[
+                {
+               model:User,through:{
+                 User_name, attributes:[]
+                  }
+                } 
+              ] 
+      });
+      res.status(200).json({ newUser });
+  
+    //   req.session.save(() => {
+    //     req.session.userId = newUser.id;
+    //     // req.session.email = newUser.email;
+    //     // req.session.loggedIn = true;
+  
+    //     res.json(newUser);
+    //   });
+    } catch (err) { 
+    console.log(err)
+    res.status(500).json(err);
+    } 
+  });
+
+  router.get('/Match', async (req, res) => {
+    try {
+      const newUser = await Match.findAll({where:{liker:1}},{
+        include:[
+           {
+              model:User
+           },
+           {
+            model:User
+         }, 
+        ] 
+      });
+      res.status(200).json({ newUser });
+  
+    //   req.session.save(() => {
+    //     req.session.userId = newUser.id;
+    //     // req.session.email = newUser.email;
+    //     // req.session.loggedIn = true;
+  
+    //     res.json(newUser);
+    //   });
+    } catch (err) { 
+    console.log(err)
+    res.status(500).json(err);
+    } 
+  });
+module.exports = router
